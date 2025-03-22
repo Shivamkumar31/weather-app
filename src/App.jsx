78e051d8b606c8becc22ec84c5da790938b07e32
shivam <shivam23bcy55@iiitkottayam.ac.in>
2025-03-22 23:29:59 +0530

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -41,17 +41,18 @@ function App() {
     // Get Current Location
     weather.fetchCurrentUserLocationData();
   
-  }, []);
-
+  }, [weather]);
+ 
   return (
     <div className="App">
       <h1>Weather Forecast</h1>
       <Input />
-      <Button onClick={weather.fetchData} value="Search" />
+      <Button onClick={weather.fetchData} value="Search" />   
       <Card />
       <Button value="Refresh" />
     </div>
   );
+
 }
 
 export default App;
